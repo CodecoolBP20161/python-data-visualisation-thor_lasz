193207c111574fd118c58cf4046538e70262152a
kalmeee <revkalman@gmail.com>
2016-07-06 13:40:30 +0200

    minimally ready image

--- sample-image-generator.py
+++ sample-image-generator.py
@@ -6,19 +6,28 @@ import math
 import main
 
 imported_dict = main.clients_name()
-img = Image.new("RGB", (520, 520), "black")
+img = Image.new("RGB", (700, 700), "black")
 draw = ImageDraw.Draw(img)
-index = 1
+x = 1
+y = 1
+y_sizes = []
 for key, val in imported_dict.items():
 
-    print (val)
+
     font = ImageFont.truetype("fonts/riesling.ttf", round(math.pow(val[0],0.2))*30)
     text_options = {'fill': val[1], 'font': font}               #szín
     text_content = key                            #szöveg
     text_size = draw.textsize(text_content, font=font)
-    print (text_size)
-    draw.text((206, index), text_content, **text_options)
-    index += (text_size[1])
-    img = img.rotate(17.5, expand=1)
+
+    if text_size[0] > 700 - x:
+        y += max(y_sizes)
+        x = 1
+        y_sizes = []
+
+    draw.text((x, y), text_content, **text_options)
+
+    x += (text_size [0] + 10)
+    y_sizes.append(text_size[1])
+
 img.save('try.png')
 img.show()
