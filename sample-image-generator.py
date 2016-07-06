4ab8c8e32875d4d51455202d792cc354dd969526
kalmeee <revkalman@gmail.com>
2016-07-06 10:20:19 +0200

    image with size and color

--- sample-image-generator.py
+++ sample-image-generator.py
@@ -1,20 +1,24 @@
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
+from PIL import ImageOps
+import math
+import main
 
-img = Image.new("RGB", (512, 512), "red")
+imported_dict = main.clients_name()
+img = Image.new("RGB", (520, 520), "black")
 draw = ImageDraw.Draw(img)
-# font = ImageFont.truetype(<font-file>, <font-size>)
-# font = ImageFont.truetype("sans-serif.ttf", 16)
-text_options = {
-    'fill': (255, 255, 255)
-}
-text_content = "Sample Text"
-text_size = draw.textsize(text_content)
-# draw.text((x, y),text_content,(r,g,b))
-draw.text((0, 0), text_content, **text_options)
-draw.text((0, text_size[1]), text_content, **text_options)
-draw.text((text_size[0], 0), text_content, **text_options)
-draw.text(text_size, text_content, **text_options)
-img.save('sample-out.png')
-# img.show()
+index = 1
+for key, val in imported_dict.items():
+
+    print (val)
+    font = ImageFont.truetype("fonts/riesling.ttf", round(math.pow(val[0],0.2))*30)
+    text_options = {'fill': val[1], 'font': font}               #szín
+    text_content = key                            #szöveg
+    text_size = draw.textsize(text_content, font=font)
+    print (text_size)
+    draw.text((206, index), text_content, **text_options)
+    index += (text_size[1])
+    img = img.rotate(17.5, expand=1)
+img.save('try.png')
+img.show()
