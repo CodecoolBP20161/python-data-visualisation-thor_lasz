35ef902d16cbec2deac51692e29b0e2251a4d736
handriss <andrashinkel@gmail.com>
2016-07-06 10:06:20 +0200

    first part colors added

--- data.py
+++ data.py
@@ -1,15 +1,7 @@
 
 
 class Record():
-    def __init__(self, id, name, duedate, company_name, company_hq, main_color, manager, budget_value, budget_currency, status, maintenance_requested):
+    def __init__(self, id, company_name, main_color):
         self.id = id
-        self.name = name
-        self.duedate = duedate
         self.company_name = company_name
-        self.company_hq = company_hq
         self.main_color = main_color
-        self.manager = manager
-        self.budget_value = budget_value
-        self.budget_currency = budget_currency
-        self.status = status
-        self.maintenance_requested = maintenance_requested
--- main.py
+++ main.py
@@ -3,13 +3,26 @@ from data import Record
 
 raw_data = connect.runSql("SELECT * FROM project")
 
+
 records = []
 
 for record in raw_data:
-    records.append(Record(record[0], record[1], record[2], record[3], record[4], record[5], record[6], record[7], record[8], record[9], record[10]))
+    records.append(Record(record[0], record[3], record[5]))
+clients = {}
 
-clients_name = {}
 for record in records:
-    clients_name[record.company_name] = [clients_name.get(record.company_name, 0) + 1,
 
-print(clients_name)
+    if record.main_color:
+        color = ''.join([x*2 for x in record.main_color[1:]])
+        color = tuple(int(color[i:i+2], 16) for i in (0, 2, 4))
+
+    if record.company_name not in clients:
+        clients[record.company_name] = [1, color]
+    else:
+        clients[record.company_name][0] += 1
+        clients[record.company_name].append(color)
+
+# print(clients)
+
+# color = "d68"
+# print(tuple(int(color[i:i+2], 16) for i in (0, 1, 2)))
