5c482d99d70bc6859d8548bcebe527099e1a715b
handriss <andrashinkel@gmail.com>
2016-07-06 10:47:19 +0200

    functionalized

--- main.py
+++ main.py
@@ -1,28 +1,42 @@
 import connect
 from data import Record
 
-raw_data = connect.runSql("SELECT * FROM project")
+def color_converter(color):
+    color = ''.join([x*2 for x in color[1:]])
+    return tuple(int(color[i:i+2], 16) for i in (0, 2, 4))
 
 
-records = []
+def clients_name():
+    # 1. Clients name:
+    raw_data = connect.runSql("SELECT * FROM project")
 
-for record in raw_data:
-    records.append(Record(record[0], record[3], record[5]))
-clients = {}
 
-for record in records:
+    records = []
+    for record in raw_data:
+        records.append(Record(record[0], record[3], record[5]))
+    clients = {}
 
-    if record.main_color:
-        color = ''.join([x*2 for x in record.main_color[1:]])
-        color = tuple(int(color[i:i+2], 16) for i in (0, 2, 4))
+    for record in records:
 
-    if record.company_name not in clients:
-        clients[record.company_name] = [1, color]
-    else:
-        clients[record.company_name][0] += 1
-        clients[record.company_name].append(color)
+        if record.main_color:
+            color = color_converter(record.main_color)
 
-# print(clients)
+        if record.company_name not in clients:
+            clients[record.company_name] = [1, color]
+        else:
+            clients[record.company_name][0] += 1
+            clients[record.company_name].append(color)
 
-# color = "d68"
-# print(tuple(int(color[i:i+2], 16) for i in (0, 1, 2)))
+    return clients
+# #-------------------------------------------------------------------------------
+#
+# # 2. Project names
+#
+# raw_data = connect.runSql("SELECT name, budget_value, main_color FROM project")
+#
+# print(raw_data)
+#
+# for record in raw_data:
+#     record[2] = color_converter(record[2])
+# #
+# # print(raw_data)
