8202365b8e7bb1dbf06a9ebf0aeb777b5b04998d
kalmeee <revkalman@gmail.com>
2016-07-07 09:55:16 +0200

    update of image generator

--- simplest_generator.py
+++ simplest_generator.py
@@ -9,23 +9,18 @@ from random import randint
 imported_dict = main.clients_name()
 img = Image.new("RGB", (700, 700), "black")
 draw = ImageDraw.Draw(img)
-counter = 0
+x = 1
+y = 1
+y_sizes = []
 
 
 for key, val in imported_dict.items():
-    x = randint(1, 699)
-    y = randint(1, 699)
-    font = ImageFont.truetype("fonts/HighlandGothicFLF.ttf", round(math.pow(val[0], 0.2)) * 30)
-    text_options = {'fill': val[1], 'font': font}  # szín
-    text_content = key  # szöveg
+
+
+    font = ImageFont.truetype("fonts/HighlandGothicFLF.ttf", round(math.pow(val[0],0.2))*30)
+    text_options = {'fill': val[1], 'font': font}               #szín
+    text_content = key                            #szöveg
     text_size = draw.textsize(text_content, font=font)
-    print ("waccap")
-
-
-    while counter < 10:
-
-        draw.text((x, y), text_content, **text_options)
-
 
     if text_size[0] > 700 - x:
         y += max(y_sizes)
